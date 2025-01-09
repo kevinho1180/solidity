d2cfd2b6f7a71926996853f2b36153c76b69efde
clonker <1685266+clonker@users.noreply.github.com>
2025-01-09 14:08:31 +0100

    cli: adds --ssa-cfg-codegen argument

--- solc/CommandLineInterface.cpp
+++ solc/CommandLineInterface.cpp
@@ -935,6 +935,7 @@ void CommandLineInterface::compile()
 		m_compiler->setRemappings(m_options.input.remappings);
 		m_compiler->setLibraries(m_options.linker.libraries);
 		m_compiler->setViaIR(m_options.output.viaIR);
+		m_compiler->setSSACFGCodegen(m_options.output.ssaCfgCodegen);
 		m_compiler->setEVMVersion(m_options.output.evmVersion);
 		m_compiler->setEOFVersion(m_options.output.eofVersion);
 		m_compiler->setRevertStringBehaviour(m_options.output.revertStrings);
@@ -950,6 +951,7 @@ void CommandLineInterface::compile()
 			pipelineConfig.irOptimization ||
 			m_options.compiler.outputs.ir ||
 			m_options.compiler.outputs.irAstJson;
+		pipelineConfig.irCodegenSSACFG = m_options.output.ssaCfgCodegen;
 		pipelineConfig.bytecode =
 			m_options.compiler.estimateGas ||
 			m_options.compiler.outputs.asm_ ||
@@ -1320,7 +1322,7 @@ void CommandLineInterface::assembleYul(yul::YulStack::Language _language, yul::Y
 
 			stack.optimize();
 
-			yul::MachineAssemblyObject object = stack.assemble(_targetMachine);
+			yul::MachineAssemblyObject object = stack.assemble(_targetMachine, m_options.output.ssaCfgCodegen);
 			if (object.bytecode)
 				object.bytecode->link(m_options.linker.libraries);
 			objects.insert({sourceUnitName, std::move(object)});
